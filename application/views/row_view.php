4ac57433b84ec94f9ccba3991cb8dc3fbd221645
Azwar Ilham <azwarilham02.mazwar892@gmail.com>
2024-06-14 13:10:11 +0800

    Menambahkan file

---
+++ application/views/row_view.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Selamat datang web saya Ilham</title>
+</head>
+
+<body>
+    <h1>selamat datang di web Ilham</h1>
+    <?= "nama saya {$row->nama_mhs} dan NIM saya {$row->NIM}"; ?>
+</body>
+
+</html>
